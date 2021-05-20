78f3dd6e71ee4d13faca5c099b90c279d8ae8032
neeraj15022001 <gneeraj32595@gmail.com>
2021-05-20 15:48:02 +0530

    added initial files

---
+++ src/components/Signup.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Form, Button , Card} from "react-bootstrap"
+
+function Signup() {
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">SignUp</h2>
+                    <Form>
+                        <Form.Group>
+                            
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an Account ? Log In 
+            </div>
+        </>
+    )
+}
+
+export default Signup
